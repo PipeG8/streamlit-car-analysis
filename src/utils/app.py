d51d948b5ba133cef5a37552f3fa297f239f8371
PipeG8 <felipe.guzman@estudiantes.uamerica.edu.co>
2026-01-31 20:41:40 -0500

    unir los codigos y llamarlos desde otro codigo

---
+++ src/utils/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv('https://raw.githubusercontent.com/anfagudelogo-tpt/datasets/refs/heads/main/car_price_dataset.csv')
+
+print(df.head())
